f32159458817d5504c4f624cf0ce21783894ec14
Samreen Islam <samreen.sfi@gmail.com>
2018-06-20 02:13:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function postComment() {
   var commenter = document.getElementById("commenterName").value;
   var comment = document.getElementById("commentText").value;
  
-  var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
   var commentTemplate = document.getElementById("comment-template").innerHTML;
  
   //create template function
