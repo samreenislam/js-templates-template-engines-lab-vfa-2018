cb4354fad7041e2120730f890795b2cbff62b0e8
Samreen Islam <samreen.sfi@gmail.com>
2018-06-20 01:12:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+<script id="page-template" type="text/x-lodash-template">
+  <div class="comment">
+    <div class="comment-body"><%- comment %></div>
+    <div class="commenter">
+      <p>
+        <span class="posted-by">Posted By: </span>
+        <%- commenter %>
+      </p>
+    </div>
+  </div>
+</script>
